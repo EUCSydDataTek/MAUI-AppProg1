2a9dbee8134c670bb27ba71259ef37acf50fe22f
EgonRasmussen <ecr@eucsyd.dk>
2022-10-05 08:48:34 +0200

    Add project files.

---
+++ AppProg1/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppProg1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ AppProg1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AppProg1;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
