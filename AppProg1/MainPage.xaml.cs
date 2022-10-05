fbb0759cbfad6a7b710a93df4f6c8e482f12b35a
EgonRasmussen <ecr@eucsyd.dk>
2022-10-05 14:42:12 +0200

    ok

--- AppProg1/MainPage.xaml.cs
+++ AppProg1/MainPage.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace AppProg1;
+﻿using AppProg1.Models;
+
+namespace AppProg1;
 
 public partial class MainPage : ContentPage
 {
 	public MainPage()
 	{
 		InitializeComponent();
-	}
+
+        // Oprettelse af Pie-objekt i koden
+        Pie pie = new Pie
+        {
+            Id = 1,
+            PieName = "Cherry Pie",
+            Price = 20
+        };
+
+        MainStackLayout.BindingContext = pie;
+
+        //this.BindingContext = pie;
+    }
 }
 
---
+++ AppProg1/Models/Pie.cs
@@ -0,0 +1,7 @@
+﻿namespace AppProg1.Models;
+public class Pie
+{
+    public int Id { get; set; }
+    public string PieName { get; set; }
+    public double Price { get; set; }
+}
