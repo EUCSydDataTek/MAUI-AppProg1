b186b402bd38f904f3d34c55c1dd55eaeb167e4b
EgonRasmussen <ecr@eucsyd.dk>
2022-11-15 13:46:27 +0100

    ok

--- AppProg1/MainPage.xaml.cs
+++ AppProg1/MainPage.xaml.cs
@@ -12,9 +12,9 @@ public partial class MainPage : ContentPage
     void OnHorizontalEndClicked(object sender, EventArgs e) { target.HorizontalOptions = LayoutOptions.End; }
     void OnHorizontalFillClicked(object sender, EventArgs e) { target.HorizontalOptions = LayoutOptions.Fill; }
 
-    void OnVerticalStartClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.Start; }
-    void OnVerticalCenterClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.Center; }
-    void OnVerticalEndClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.End; }
-    void OnVerticalFillClicked(object sender, EventArgs e) { target.VerticalOptions = LayoutOptions.Fill; }
+    void OnVerticalStartClicked(object sender, EventArgs e) { verticalLayout.VerticalOptions = LayoutOptions.Start; }
+    void OnVerticalCenterClicked(object sender, EventArgs e) { verticalLayout.VerticalOptions = LayoutOptions.Center; }
+    void OnVerticalEndClicked(object sender, EventArgs e) { verticalLayout.VerticalOptions = LayoutOptions.End; }
+    void OnVerticalFillClicked(object sender, EventArgs e) { verticalLayout.VerticalOptions = LayoutOptions.Fill; }
 }
 
