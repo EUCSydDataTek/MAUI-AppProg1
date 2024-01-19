0c88869838089d130ce237e6d39877287acf011d
EgonRasmussen <ecr@eucsyd.dk>
2024-01-19 14:07:09 +0100

    Ekstra ValueConverter

---
+++ AppProg1/Converters/BoolColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace AppProg1.Converters;
+public class BoolColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool)
+        {
+            if ((bool)value)
+            {
+                return Colors.Green;
+            }
+            else
+            {
+                return Colors.Red;
+            }
+        }
+        return Colors.Black;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
