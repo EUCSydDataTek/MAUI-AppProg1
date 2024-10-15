b8176b1ac84c931cf32e2165ccff7e6e94521f83
EgonRasmussen <ecr@eucsyd.dk>
2024-10-15 13:55:47 +0200

    ok

--- AppProg1/Models/Monkey.cs
+++ AppProg1/Models/Monkey.cs
@@ -4,5 +4,5 @@ public class Monkey
     public string Name { get; set; }
     public string Location { get; set; }
     public string Details { get; set; }
-    public string ImageUrl { get; set; }
+    public string Image { get; set; }
 }
