554222dcc3c879af7f379a7869d889af1371b392
EgonRasmussen <ecr@eucsyd.dk>
2025-02-06 15:34:57 +0100

    .NET9

--- AppProg1/App.xaml.cs
+++ AppProg1/App.xaml.cs
@@ -2,12 +2,13 @@
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
+    }
 
-		//MainPage = new AppShell();
-
-		MainPage = new NavigationPage(new MainPage());
-	}
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        return new Window(new NavigationPage(new MainPage()));
+    }
 }
