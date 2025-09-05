abe49d3cc4cba1107fe0cb6d43f3ffd15ece6101
EgonRasmussen <ecr@eucsyd.dk>
2025-09-05 12:57:58 +0200

    ok

--- AppProg1/MauiProgram.cs
+++ AppProg1/MauiProgram.cs
@@ -17,7 +17,6 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
-        builder.Services.AddSingleton<MainPage>();
         builder.Services.AddSingleton<MainPageViewModel>();
 
 #if DEBUG
