ae75383a7610e8053315f07023f02840cc0d7358
EgonRasmussen <ecr@eucsyd.dk>
2025-09-05 13:28:55 +0200

    ok

--- AppProg1/ViewModels/BaseViewModel.cs
+++ AppProg1/ViewModels/BaseViewModel.cs
@@ -33,11 +33,7 @@ public class BaseViewModel : INotifyPropertyChanged
     public event PropertyChangedEventHandler PropertyChanged;
     protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
     {
-        var changed = PropertyChanged;
-        if (changed == null)
-            return;
-
-        changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
     #endregion
 }
